f5ca6345743cacad62de1be1086fa8244793b892
luisclaudiorp <61363429+luisclaudiorp@users.noreply.github.com>
2022-02-28 18:17:02 -0300

    Exercicio 4 HTML

--- exercicios/exercicioQuatro.js
+++ exercicios/exercicioQuatro.js
@@ -1,6 +1,14 @@
-function mediaAritimetica(num1, num2, num3) {
-    const calculo = (num1 + num2 + num3) / 3
-    return `A média aritimética dos números ${num1}, ${num2} e ${num3} é igual à: ${calculo}`
-}
+// function mediaAritimetica(num1, num2, num3) {
+//     const calculo = (num1 + num2 + num3) / 3
+//     return `A média aritimética dos números ${num1}, ${num2} e ${num3} é igual à: ${calculo}`
+// }
 
-console.log(mediaAritimetica(10, 20, 30))
+// console.log(mediaAritimetica(10, 20, 30))
+
+function mediaAritmetica() {
+    const num1 = document.getElementById("num1").value
+    const num2 = document.getElementById("num2").value
+    const num3 = document.getElementById("num3").value
+    const calculo = (parseFloat(num1) + parseFloat(num2) + parseFloat(num3)) / 3
+    document.getElementById("mediaAritmetica").innerHTML = `A média aritmética dos números anteriores é ${calculo.toFixed(2)}`
+}
