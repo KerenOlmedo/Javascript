881adfe013c57cd1c16aca4d1a4c682b885e5d61
luisclaudiorp <61363429+luisclaudiorp@users.noreply.github.com>
2022-02-28 16:29:57 -0300

    Exercicio 3 html

--- exercicios/exercicioTres.js
+++ exercicios/exercicioTres.js
@@ -1,13 +1,18 @@
-const valCombustivel = 6.99
-const dinheiro = 40.00
 //const litros = dinheiro / valCombustivel
 //const kmCarroAnda = litros * 20
 //console.log(`Com R$${dinheiro} é possível comprar ${litros} litros de combustível. Seu carro andará ${kmCarroAnda} Km com esta quantidade.`)
 
-function calculoValCombustivel (dinheiro, valCombustivel){
-    const litros = dinheiro / valCombustivel
+function calculoValCombustivel (){
+    const valCombustivel = document.getElementById("valCombustivel").value
+    const QuantDinheiro = document.getElementById("QuantDinheiro").value
+    const litros = QuantDinheiro / valCombustivel
     const kmCarroAnda = litros * 20
-    return `Com R$${dinheiro} é possível comprar ${litros.toFixed(2)} litros de combustível. Seu carro andará ${kmCarroAnda.toFixed(2)} Km com esta quantidade.`
+    //console.log(litros.toFixed(2), kmCarroAnda.toFixed(2))
+    document.getElementById("litros").innerHTML = `${litros.toFixed(2)} Litros`
+    document.getElementById("km").innerHTML = `${kmCarroAnda.toFixed(2)} Km`
+    // const litros =  / valCombustivel
+    // const kmCarroAnda = litros * 20
+    // return `Com R$${dinheiro} é possível comprar ${litros.toFixed(2)} litros de combustível. Seu carro andará ${kmCarroAnda.toFixed(2)} Km com esta quantidade.`
 }
 
-console.log(calculoValCombustivel(50,6.99))
+//console.log(calculoValCombustivel(50,6.99))
