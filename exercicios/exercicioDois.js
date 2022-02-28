e793661068d63b5686b45e9c14c530fcf7a3d276
luisclaudiorp <61363429+luisclaudiorp@users.noreply.github.com>
2022-02-28 15:27:55 -0300

    exercicio 2 HTML

--- exercicios/exercicioDois.js
+++ exercicios/exercicioDois.js
@@ -2,9 +2,15 @@
 // const formula = grausCelsius * 9 / 5 + 32
 // console.log(`${grausCelsius} graus Celsius é igual à: ${formula} graus Fahrenheit`)
 
-function fahrenheit(grausCelsius) {
-    const calculo = grausCelsius * 9 / 5 + 32
-    return `${grausCelsius} graus Celsius é igual à: ${calculo} graus Fahrenheit`
-}
+// function fahrenheit(grausCelsius) {
+//     const calculo = grausCelsius * 9 / 5 + 32
+//     return `${grausCelsius} graus Celsius é igual à: ${calculo} graus Fahrenheit`
+// }
 
-console.log(fahrenheit(4))
+// console.log(fahrenheit(4))
+
+function calculoConversao() {
+    const temperatura = document.getElementById("temperatura").value
+    const calculo = (temperatura * 1.8) + 32
+    document.getElementById("fahrenheit").innerHTML = "Temperatura em Fahrenheit " + calculo
+}
