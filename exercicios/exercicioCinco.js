a970437959b5d3903b038bed30fb0f3208ae05cd
luisclaudiorp <61363429+luisclaudiorp@users.noreply.github.com>
2022-03-01 15:02:50 -0300

    Exercicio 5 HTML

--- exercicios/exercicioCinco.js
+++ exercicios/exercicioCinco.js
@@ -1,9 +1,19 @@
-function idadeMedia(idade) {
+// function idadeMedia(idade) {
+//     if (idade >= 18) {
+//         return `O  usuário é maior de idade.`
+//     } else {
+//         return `O usuário é menor de idade.`
+//     }
+// }
+
+// console.log(idadeMedia(19))
+
+function idadeMedia() {
+    const idade = document.getElementById("idade").value
     if (idade >= 18) {
-        return `O  usuário é maior de idade.`
+        document.getElementById("idadeMedia").innerHTML = `Você é maior de idade.`
     } else {
-        return `O usuário é menor de idade.`
+        document.getElementById("idadeMedia").innerHTML = `Você é menor de idade.`
     }
 }
 
-console.log(idadeMedia(19))
