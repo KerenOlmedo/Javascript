2d35dc710ec0e73eebac2f49b1bf9f17a78cadef
luisclaudiorp <61363429+luisclaudiorp@users.noreply.github.com>
2022-03-01 15:37:44 -0300

    Exercicio 6 HTML

--- exercicios/exercicioSeis.js
+++ exercicios/exercicioSeis.js
@@ -1,13 +1,15 @@
-function numeroMaior(num1, num2, num3) {
+function numeroMaior() {
+    const num1 = parseInt(document.getElementById("num1").value)
+    const num2 = parseInt(document.getElementById("num2").value)
+    const num3 = parseInt(document.getElementById("num3").value)
     if (num1 > num2 & num1 > num3) {
-        return `O número ${num1} é maior que os números ${num2} e ${num3}.`
+        document.getElementById("numeroMaior").innerHTML = `O número ${num1} é maior que os números ${num2} e ${num3}.`
     }else if (num2 > num1 & num2 > num3){
-        return `O número ${num2} é maior que os números ${num1} e ${num3}.`
+        document.getElementById("numeroMaior").innerHTML = `O número ${num2} é maior que os números ${num1} e ${num3}.`
     }else{
-        return `O número ${num3} é maior que os números ${num1} e ${num2}.`
+        document.getElementById("numeroMaior").innerHTML = `O número ${num3} é maior que os números ${num1} e ${num2}.`
     }
 }
-
-console.log(numeroMaior(3, 2, 1))
-console.log(numeroMaior(2, 3, 1))
-console.log(numeroMaior(1, 2, 3))
+// console.log(numeroMaior(3, 2, 1))
+// console.log(numeroMaior(2, 3, 1))
+// console.log(numeroMaior(1, 2, 3))
