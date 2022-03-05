aa044be179e48326e478f9cb387036d18a053f0f
Kéren Olmedo <keren.olmedo09@gmail.com>
2022-03-05 00:26:04 -0300

    exercicio 8 HTML

--- exercicios/exercicioOito.js
+++ exercicios/exercicioOito.js
@@ -1,8 +1,17 @@
-function sequencial(caracter) {
-    const ordenado = caracter.sort(function(a, b){
-        return a - b
-    })
-    return `Lista original: ${caracter} lista em sequencia: ${ordenado}`
-}
+// function sequencial(caracter) {
+//     const ordenado = caracter.sort(function(a, b){
+//         return a - b
+//     })
+//     return `Lista original: ${caracter} lista em sequencia: ${ordenado}`
+// }
 
-console.log(sequencial([10, 50, 100, 20]))
+// console.log(sequencial([10, 50, 100, 20]))
+
+function sequencial() {
+    const listaOriginal = document.getElementById("listaAleatoria").value
+    const listaOrdenada = listaOriginal.sort(function(a, b){
+       return a - b})
+    document.getElementById("listaOriginal").innerHTML = listaOriginal
+    document.getElementById("ListaOrdenada").innerHTML = listaOrdenada
+    console.log(listaOriginal, listaOrdenada)
+}
